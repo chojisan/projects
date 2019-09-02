bd10128e39aafd2b53e1978a4e22730f743ba2af
Seigi Takada <seigi.takada@CPHITPC5.contexta.co>
2019-09-02 12:23:52 +0800

    Fixed projects index page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,11 +1,73 @@
-@extends('layout')
+@extends('layouts.admin')
 
-@section('contents')
-<h1>Projects</h1>
+@section('content-header')
+    <section class="content-header">
+        <h1>
+            Projects
+            <small>list of projects</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+            <li><a href="{{ route('projects.index') }}" class="active"><i class="fa fa-cubes"></i> Projects</a></li>
+        </ol>
+    </section>
+@endsection
 
-<ul>
-@foreach ($projects as $project)
-    <li>{{ $project->title }}</li>
-@endforeach
-</ul>
+@section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Projects Table</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <a href="{{ route('projects.create') }}" class="btn btn-primary" style="margin-bottom: 10px;">Add Project</a>
+                        <table class="table table-bordered">
+                            <tr>
+                                <th style="width: 10px">#</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th style="width: 40px"></th>
+                                <th style="width: 40px"></th>
+                                <th style="width: 40px"></th>
+                            </tr>
+
+                            @foreach($projects as $project)
+                                <tr>
+                                    <td>{{ $project->id }}</td>
+                                    <td>{{ $project->title }}</td>
+                                    <td>{{ $project->description }}</td>
+                                    <td><a href="{{ route('projects.show', $project->id) }}"><i class="fa fa-eye"></i></a></td>
+                                    <td><a href="{{ route('projects.edit', $project->id) }}"><i class="fa fa-edit"></i></a></td>
+                                    <td>
+                                        <a href="/users/{{ $project->id }}" onclick="event.preventDefault();
+                                        document.getElementById('delete-user-form-{{ $project->id }}').submit();">
+                                            <i class="fa fa-trash"></i>
+                                        </a>
+                                        <form id="delete-user-form-{{ $project->id }}" action="{{ route('projects.destroy', $user->id) }}" method="POST" style="display: none;">
+                                            @csrf
+                                             @method('DELETE')
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer clearfix">
+                        <ul class="pagination pagination-sm no-margin pull-right">
+                            <li><a href="#">&laquo;</a></li>
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">&raquo;</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.box -->
+            </div>
+        </div>
+    </section>
 @endsection
