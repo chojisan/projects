b38a35b2689ce64b9a89a404a025c687305e4940
Seigi Takada <seigi.takada@CPHITPC5.contexta.co>
2019-09-02 12:23:28 +0800

    Added projects menu item

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,40 +2,45 @@
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-      <!-- Sidebar user panel -->
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img src="img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="img/user2-160x160.jpg" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p>{{ Auth::user()->name }}</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
         </div>
-        <div class="pull-left info">
-          <p>{{ Auth::user()->name }}</p>
-          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-      <!-- /.search form -->
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MAIN NAVIGATION</li>
-        <li class="{{ (request()->routeIs('dashboard')) ? 'active' : '' }}">
-          <a href="{{ route('dashboard') }}">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-          </a>
-        </li>
-        <li class="{{ (request()->routeIs('users*')) ? 'active' : '' }}">
-          <a href="{{ route('users.index') }}">
-              <i class="fa fa-users"></i> <span>Users</span>
-            </a>
-          </li>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="{{ (request()->routeIs('dashboard')) ? 'active' : '' }}">
+                <a href="{{ route('dashboard') }}">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="{{ (request()->routeIs('projects*')) ? 'active' : '' }}">
+                <a href="{{ route('projects.index') }}">
+                    <i class="fa fa-cubes"></i> <span>Projects</span>
+                </a>
+            </li>
+            <li class="{{ (request()->routeIs('users*')) ? 'active' : '' }}">
+                <a href="{{ route('users.index') }}">
+                    <i class="fa fa-users"></i> <span>Users</span>
+                </a>
+            </li>
         <!--
         <li class="treeview">
           <a href="#">
