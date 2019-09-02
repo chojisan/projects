9d9f26b161e887f6dd029252bfbe702b2615f073
Seigi Takada <seigi.takada@CPHITPC5.contexta.co>
2019-09-03 06:57:30 +0800

    Added flash message template

---
+++ resources/views/flash-message.blade.php
@@ -0,0 +1,44 @@
+
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block alert-dismissible flash-message">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-check"></i> Alert!</h4>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($message = Session::get('error'))
+    <div class="alert alert-danger alert-block alert-dismissible flash-message">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($message = Session::get('warning'))
+    <div class="alert alert-warning alert-block alert-dismissible flash-message">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>	
+        <h4><i class="icon fa fa-warning"></i> Alert!</h4>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($message = Session::get('info'))
+    <div class="alert alert-info alert-block alert-dismissible flash-message">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-info"></i> Alert!</h4>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($errors->any())
+    <div class="alert alert-danger alert-dismissible flash-message">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+        Please check the form below for errors
+    </div>
+@endif
