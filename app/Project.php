cbe461c33c16e6dabca72a4c232b3983330bd7cf
Seigi Takada <seigi.takada@CPHITPC5.contexta.co>
2019-09-03 14:36:49 +0800

    Added user project relations

--- app/Project.php
+++ app/Project.php
@@ -9,4 +9,9 @@ class Project extends Model
     protected $fillable = [
         'title', 'description'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
